b7cd45fc2886e2009c0436e63b8244151fcb212d
Daberechi <daberelevi@gmail.com>
2025-06-19 22:30:28 +0100

    Updated

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,43 +4,43 @@ import Certifications from "./Certifications";
 import TechnicalSkills from "./TechnicalSkills";
 import "../app/sidebar.css";
 
-const achievements = [
-  {
-    title: "Developed High-Performance System",
-    description:
-      "Led a team to deliver a system with 20% increased uptime, optimizing scalability and performance for enterprise clients.",
-  },
-  {
-    title: "Reduced Bug Rate",
-    description:
-      "Mentored junior engineers, resulting in a 25% bug reduction through improved best coding practices and code reviews.",
-  },
-  {
-    title: "Enhanced User Interaction",
-    description:
-      "Implemented features that improved user interaction by 30%, increasing customer satisfaction and engagement.",
-  },
-  {
-    title: "Improved System Response",
-    description:
-      "Devised monitoring strategies that reduced response time to system issues by 50%, improving operational efficiency.",
-  },
-  {
-    title: "Security Compliance Champion",
-    description:
-      "Integrated robust security standards, reducing vulnerabilities by 35% and ensuring compliance with industry regulations.",
-  },
-  {
-    title: "Cross-Functional Collaboration",
-    description:
-      "Facilitated collaboration between design, product, and engineering teams, accelerating project delivery and innovation.",
-  },
-];
+// const achievements = [
+//   {
+//     title: "Developed High-Performance System",
+//     description:
+//       "Led a team to deliver a system with 20% increased uptime, optimizing scalability and performance for enterprise clients.",
+//   },
+//   {
+//     title: "Reduced Bug Rate",
+//     description:
+//       "Mentored junior engineers, resulting in a 25% bug reduction through improved best coding practices and code reviews.",
+//   },
+//   {
+//     title: "Enhanced User Interaction",
+//     description:
+//       "Implemented features that improved user interaction by 30%, increasing customer satisfaction and engagement.",
+//   },
+//   {
+//     title: "Improved System Response",
+//     description:
+//       "Devised monitoring strategies that reduced response time to system issues by 50%, improving operational efficiency.",
+//   },
+//   {
+//     title: "Security Compliance Champion",
+//     description:
+//       "Integrated robust security standards, reducing vulnerabilities by 35% and ensuring compliance with industry regulations.",
+//   },
+//   {
+//     title: "Cross-Functional Collaboration",
+//     description:
+//       "Facilitated collaboration between design, product, and engineering teams, accelerating project delivery and innovation.",
+//   },
+// ];
 
 const Sidebar = () => {
   return (
     <aside className="sidebar">
-      <section className="sidebar-section achievements">
+      {/* <section className="sidebar-section achievements">
         <h2>Key Achievements</h2>
         <ul>
           {achievements.map((item, idx) => (
@@ -50,7 +50,7 @@ const Sidebar = () => {
             </li>
           ))}
         </ul>
-      </section>
+      </section> */}
       <section className="sidebar-section">
         <h2>Projects</h2>
         <Projects />
