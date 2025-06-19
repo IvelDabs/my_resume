3937e16e1e3b98f9473f29608daa732ad66705e0
Daberechi <daberelevi@gmail.com>
2025-06-19 22:33:21 +0100

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import Summary from "../components/Summary";
-import SoftSkills from "../components/SoftSkills";
+// import SoftSkills from "../components/SoftSkills";
 import Experience from "../components/Experience";
 import Education from "../components/Education";
 import Sidebar from "../components/Sidebar";
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-8">
         <main>
           <Summary />
-          <SoftSkills />
+          {/* <SoftSkills /> */}
           <Experience />
           <Education />
         </main>
